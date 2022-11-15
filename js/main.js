0ac07d70139adeed60fb93ea24c1916820fa1ddb
Daniel-Rue <78970069+Daniel-Rue@users.noreply.github.com>
2022-11-15 21:28:30 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-btn');
+const navBtnImg = document.querySelector('#nav-btn-img');
+
+navBtn.onclick = () => {
+    if (nav.classList.toggle('open')) {
+        navBtnImg.src = "./image/nav-close.svg";
+    } else {
+        navBtnImg.src = "./image/nav-open.svg";
+    }
+}
+
+const anchors = document.querySelectorAll('a[href^="#"]')
+for(let anchor of anchors) {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault() 
+    const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
+    document.querySelector(goto).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    })
+  })
+}
+
+AOS.init();
